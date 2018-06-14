0e6b7c8523516494d288c264f7512f54b3f76530
sowmya devathu <sowmyad.qa@gmail.com>
2018-06-14 17:11:53 -0400

    Update project.py

--- project.py
+++ project.py
@@ -427,4 +427,4 @@ def deleteMenuItem(category_name, item_name):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    #app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
